339ac3151cf7bdbfc1d30724d3a2bf71cc929dbc
WJX2001 <1758807220@qq.com>
2023-05-20 17:18:41 +0800

    feat: react-book

--- react-book/src/App.js
+++ react-book/src/App.js
@@ -1,45 +1,17 @@
-import { Component } from "react";
+import React,{Component} from "react";
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
+import './index.css'
 
-
-class LikeButton extends Component {
-  constructor() {
-    super()
-    this.state = {
-      
+class App extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <CommentInput />
+                <CommentList />
+            </div>
+        )
     }
-  }
-
-  handleClickOnLikeButton = () => {
-    this.setState((prevState) => {
-      return {count:0}
-    })
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 } // 上一个 setState 的返回是 count 为 0，当前返回 1
-    })
-    this.setState((prevState) => {
-      return { count: prevState.count + 2 } // 上一个 setState 的返回是 count 为 1，当前返回 3
-    })
-    console.log(this.state.count)
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClickOnLikeButton}>count: {this.state.count}</button>
-      </div>
-    )
-  }
-}
-
-
-
-class App extends Component{
-  render() {
-    return (
-       <div>
-          <LikeButton />
-       </div>
-    );
-  }
 }
 
 export default App
