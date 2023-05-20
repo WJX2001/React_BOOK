dc8705b2765f2a5355e1e9dc1d997dc67a909d81
WJX2001 <1758807220@qq.com>
2023-05-20 20:08:14 +0800

    feat: 简易的评论功能

--- react-book/src/CommentInput.js
+++ react-book/src/CommentInput.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react'
 class CommentInput extends Component {
+
+  state = {
+    username:"",
+    content: ''
+  }
+  
+  handleUsernameChange = (e) => {
+    this.setState({
+      username:e.target.value
+    })
+  }
+  handleContentChange = (e) => {
+    this.setState({
+      content:e.target.value
+    })
+  }
+  handleSubmit = () => {
+    // 通过方法判断props是否传入onSubmit属性
+    if(this.props.onSubmit) {
+      const {username,content} = this.state
+      this.props.onSubmit({username,content})
+    }
+    // setState清空用户输入的评论内容（为了用户体验，保留输入的用户名）
+    this.setState({
+      content:''
+    })
+  }
+
+
   render() {
     return (
       <div className="comment-input">
         <div className="comment-field">
           <span className="comment-field-name">用户名</span>
           <div className="comment-field-input">
-            <input />
+            <input 
+              value={this.state.username} 
+              onChange={this.handleUsernameChange}
+            />
           </div>
         </div>
         <div className="comment-field">
           <span className="comment-field-name">评论内容：</span>
           <div className="comment-field-input">
-            <textarea />
+            <textarea 
+              value={this.state.content}
+              onChange={this.handleContentChange}
+              />
           </div>
         </div>
         <div className="comment-field-button">
-          <button>发布</button>
+          <button 
+            onClick={this.handleSubmit}>
+              发布
+          </button>
         </div>
       </div>
     )
